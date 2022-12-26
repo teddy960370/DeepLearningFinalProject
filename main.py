fbf62a6cc4f353c439d8872611b248c39fedeebb
Ted Lee <dirty4538@gmail.com>
2022-12-26 19:04:46 +0800

    test

--- main.py
+++ main.py
@@ -14,6 +14,9 @@ from traceingAnimate import animate_player_movement
 from PreProcess import preProcess
 from model import NFL_LSTM_classifier
 from tqdm import trange
+from dataset import NFL_Track_Dataset
+from torch.utils.data import DataLoader
+from sklearn.model_selection import train_test_split
 
 import torch
 
@@ -23,20 +26,32 @@ def main():
     
     path = './data/'
 
+    df_games = pd.read_csv(path + 'games.csv')
+    df_plays = pd.read_csv(path + 'plays.csv')
+    df_players = pd.read_csv(path +'players.csv')
+    df_pffScouting = pd.read_csv(path +'pffScoutingData.csv')
+    
+    df_tracking  = pd.DataFrame()
+    
+    for i in range(1,9,1):
+        buffer = pd.read_csv(path + 'week' + str(i) + '.csv')
+        df_tracking = pd.concat([df_tracking,buffer])
+        #df_tracking.append(buffer, ignore_index=True)
+
+    # 找出各play的frame數
+    #df_tracking_temp = df_tracking[['gameId','playId','frameId']].groupby(['gameId','playId']).max()
+    # 移除大於64的結果
+    #df_tracking_temp = df_tracking_temp.loc[df_tracking_temp['frameId'] <= 64]
+    
+    df_gp = df_plays[['gameId','playId','possessionTeam','defensiveTeam','passResult']]
+    train,vaild = train_test_split(df_gp, test_size=0.3)
+
+    batch_size = 32
+    train_dataset = NFL_Track_Dataset(train,df_games,df_pffScouting,df_players,df_tracking,'train')
+    train_loader = DataLoader(train_dataset, batch_size = batch_size, shuffle = False,collate_fn=train_dataset.collate_fn)
+    
+    
 # =============================================================================
-#     df_games = pd.read_csv(path + 'games.csv')
-#     df_plays = pd.read_csv(path + 'plays.csv')
-#     df_players = pd.read_csv(path +'players.csv')
-#     df_pffScouting = pd.read_csv(path +'pffScoutingData.csv')
-#     
-#     df_tracking  = pd.DataFrame()
-#     
-#     for i in range(1,9,1):
-#         buffer = pd.read_csv(path + 'week' + str(i) + '.csv')
-#         df_tracking = pd.concat([df_tracking,buffer])
-#         #df_tracking.append(buffer, ignore_index=True)
-# 
-#         
 #     # select random play
 #     df_sample_plays = df_plays.iloc[100]
 #     gameID = df_sample_plays['gameId']
@@ -72,9 +87,9 @@ def main():
     
     for epoch in epoch_pbar:
         # TODO: Training loop - iterate over train dataloader and update model weights
-        train_loop()
+        train_loop(train_loader,model, optimizer,intput_data_length,output_size)
         # TODO: Evaluation loop - calculate accuracy and save model weights
-        vaild_loop()
+        #vaild_loop()
         pass
 
     
@@ -89,8 +104,11 @@ def main():
     video.save(path + 'video.mp4')
 
 
-def train_loop():
-    raise NotImplementedError
+def train_loop(dataloader,model,optimizer,intput_data_length,output_size):
+    
+    for data in dataloader:
+        print(data)
+    
 
 def vaild_loop():
     
